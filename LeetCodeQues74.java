4fdf7a09e4575a6b1d89483f95cfd0e230147455
Akshat Bisht <109125241+Kaisama@users.noreply.github.com>
2023-08-17 20:00:48 +0530

    LeetCodeQues74.java

---
+++ LeetCodeQues74.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+         int m=matrix.length;
+        int n=matrix[0].length;
+
+        int start=0;
+        int end =(n*m)-1;
+        while(start<=end)
+        {
+            int mid = start+(end-start)/2;
+            if(matrix[mid/n][mid%n]==target)
+            {
+                return true;
+            }
+            else if(matrix[mid/n][mid%n]<target)
+            {
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return false;
+   
+    }
+}
