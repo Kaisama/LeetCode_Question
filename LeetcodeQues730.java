87b68a6f0b8c0ea1ea0d36b72631bf113f1b3f28
Akshat Bisht <109125241+Kaisama@users.noreply.github.com>
2023-08-17 19:55:51 +0530

    LeetcodeQues730.java

---
+++ LeetcodeQues730.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int countPalindromicSubsequences(String s) {
+        int big = 1000000007;
+        int len = s.length();
+        if (len < 2) {
+            return len;
+        }
+        int[][] dp = new int[len][len];
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            int deta = 1;
+            dp[i][i] = 1;
+            int l2 = -1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (s.charAt(j) == c) {
+                    if (l2 < 0) {
+                        l2 = j;
+                        deta = dp[j + 1][i - 1] + 1;
+                    } else {
+                        deta = dp[j + 1][i - 1] - dp[j + 1][l2 - 1];
+                    }
+                    deta = deal(deta, big);
+                }
+                dp[j][i] = deal(dp[j][i - 1] + deta, big);
+            }
+        }
+        return deal(dp[0][len - 1], big);
+    }
+    private int deal(int x, int big) {
+        x %= big;
+        if (x < 0) {
+            x += big;
+        }
+        return x;
+    }
+}
