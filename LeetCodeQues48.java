14a79eb215143d8e18ccedb9f63c3122901bd83e
Akshat Bisht <109125241+Kaisama@users.noreply.github.com>
2023-08-17 19:59:07 +0530

    LeetCodeQues48.java

---
+++ LeetCodeQues48.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int[][] rotated = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotated[j][n - i - 1] = matrix[i][j];
+            }
+        }
+        // Copy rotated matrix back to the original matrix
+        for (int i = 0; i < n; i++) {
+            System.arraycopy(rotated[i], 0, matrix[i], 0, n);
+        }
+    }
+}
